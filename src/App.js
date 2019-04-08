bd405f1f73c5cb6f50d633adbd05fc904ad24c04
morcillo <morcillomuriel@gmail.com>
2019-04-08 13:35:37 +0200

    challenge react3 le state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Quotes from "./Quotes";
+import Quote from "./Quote";
+import Lamp from "./Lamp";
+import Working from "./Working";
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      open : false
+    };
+  }
+  toggleOpenState(){
+    this.setState({ open : !this.state.open})
+  }
+  render(){  
+    const imgClass = this.state.open ? 'open' : 'close';
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className={`container ${imgClass}`}>
+          <img className={imgClass} src="https://cdn.pixabay.com/photo/2013/11/28/11/29/soccer-ball-220205_960_720.jpg" />
+          <h1 className="openClose">SIMPSONS QUOTES</h1>
+
+        </div>
+        <button
+        onClick={this.toggleOpenState.bind(this)}
+        
+        >Click Me !</button>
+        <Lamp on />
+        <Lamp />
+        <Quotes/>
       </div>
     );
   }
